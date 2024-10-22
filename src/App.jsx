83f5f0e24ad7b7f64172fb2ca871737ab5f5293d
“Riwaj” <shresthariwaj10@gmail.com>
2024-10-22 10:19:36 +0545

    pixpoint landing page

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Navigation from "./components/Navigation"
+import Newsletter from "./components/Newsletter"
+
+import './index.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <Navigation />
+      <Newsletter />
+      <Footer />
+    </>
+  )
+}
+
+export default App
